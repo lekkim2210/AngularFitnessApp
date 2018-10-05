7a7a2e40b113f8978b9a0c44d6c423886f9c8c76
mikkel bitsch <mikbitsch@live.dk>
2018-10-05 09:49:24 +0200

    Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
